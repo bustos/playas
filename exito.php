6f589f6867ae210c1492e291d5f6d516e708315a
Carlos J. Bustos Tore <bustos@hotmail.com>
2013-03-13 17:52:42 +0100

    nuevo

---
+++ exito.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+
+
+
+<?php
+$code=$_GET["code"];
+echo "<form action='ciudades.php?code=".$code."' method='post'>";
+
+echo "Exito";	
+	
+?>
+     
+<INPUT type="text" name="code"><BR>
+<input type="submit" value="Pulsa para volver"><br/><br/>
+</form>
+
+</body>
+</html>
