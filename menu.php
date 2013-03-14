c756162bb51899fcac133d32f25b95060960a98d
Carlos J. Bustos Tore <bustos@hotmail.com>
2013-03-14 18:16:33 +0100

    13/03/2013

---
+++ menu.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+
+<form>
+
+<?php
+	echo '<a href="citys.php?code=">Ciudades</a>        ';	
+	echo '   <a href="countrys.php?code=">Paises</a>';	
+ ?>
+     
+
+
+</form>
+
+</body>
+</html>
